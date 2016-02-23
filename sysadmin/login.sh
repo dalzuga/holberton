6ae4310fd78bf23f6203b9ce0cb837f1a67c6317
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-22 17:39:14 -0800

    asdf

--- sysadmin/login.sh
+++ sysadmin/login.sh
@@ -9,5 +9,11 @@ mkdir -p /Users/Shared/${user}/
 
 if [ -f /Users/Shared/${user}/.bash_profile ];
 then
-    cp /Users/Shared/${user}/.bash_profile /Users/${user}/.bash_profile
+    cp /Users/Shared/${user}/.bash_profile /Users/${user}/.bash_profile # fetch .bash_profile
+fi
+
+if [ -x /Users/Shared/${user}/logonscript.sh ];
+then
+    cp /Users/Shared/${user}/logonscript.sh /Users/${user}/logonscript.sh # fetch custom log on script
+    /Users/${user}/logonscript.sh # execute script
 fi
--- sysadmin/logout.sh
+++ sysadmin/logout.sh
@@ -9,5 +9,10 @@ mkdir -p /Users/Shared/${user}/
 
 if [ -f /Users/Shared/${user}/.bash_profile ];
 then
-    cp /Users/${user}/.bash_profile /Users/Shared/${user}/
+    cp /Users/${user}/.bash_profile /Users/Shared/${user}/ # save .bash_profile on logout
+fi
+
+if [ -x /Users/${user}/logonscript.sh ];
+then
+    cp /Users/${user}/logonscript.sh /Users/Shared/${user}/logonscript.sh # save custom script on logout
 fi

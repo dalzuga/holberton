f845156047dc71ec56af70c4ccff54448af6837a
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-22 17:17:10 -0800

    asdf

---
+++ sysadmin/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#ssh-keygen -t rsa -b 4096 -C "dalzuga@gmail.com"
+#cat ~/.ssh/id_rsa.pub
+#open https://github.com/settings/ssh
+
+PS1="\[\e[0;36m\]\h\[\e[m\]:\W \[\e[34m\]\u\[\e[m\]\[\e[31m\]\$\[\e[m\] "
+
+git config --global user.name "Daniel Alzugaray"
+git config --global user.email "dalzuga@gmail.com"
+git config --global core.editor emacs
+git config --global push.default simple
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci "commit -a -m"
+git config --global alias.st status
